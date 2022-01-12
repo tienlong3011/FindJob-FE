706f84bc241a4bb8389c367fc6e6fbf0220c7408
ngdloc2000 <ngdinhloc2000@gmail.com>
2022-01-13 01:27:18 +0700

    tao component update tin tuyển dụng

---
+++ src/app/company/recruitmentnew/update-recruitmentnew-company/update-recruitmentnew-company.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-recruitmentnew-company',
+  templateUrl: './update-recruitmentnew-company.component.html',
+  styleUrls: ['./update-recruitmentnew-company.component.scss']
+})
+export class UpdateRecruitmentnewCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
